b00b258f08c8b387a4a2c1c80211fd652b0637bb
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-09-17 12:46:54 -0700

    includes complete MockLoader class

--- AstronomyTests/MarsRoverClientTest.swift
+++ AstronomyTests/MarsRoverClientTest.swift
@@ -7,9 +7,39 @@
 //
 
 import XCTest
+@testable import Astronomy
+
+class MockLoader: NetworkDataLoader {
+    
+    init(data: Data?, error: Error?) {
+        self.data = data
+        self.error = error
+    }
+    
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        self.request = request
+        DispatchQueue.global().async {
+            completion(self.data, self.error)
+        }
+    }
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        self.url = url
+        DispatchQueue.global().async {
+            completion(self.data, self.error)
+        }
+    }
+    
+    let data: Data?
+    let error: Error?
+    private(set) var request: URLRequest? = nil
+    private(set) var url: URL? = nil
+}
 
 class MarsRoverClientTest: XCTestCase {
-        
     
+    
+    // test fetchMarsRover()
+    // test fetchPhotos
     
 }
