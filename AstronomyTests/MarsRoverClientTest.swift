d60f04e1d1a9b7dcb6a3e14ab6609ddea755c143
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-09-17 12:21:03 -0700

    implements NetworkDataLoader protocol and URLSession extension

---
+++ AstronomyTests/MarsRoverClientTest.swift
@@ -0,0 +1,15 @@
+//
+//  MarsRoverClientTest.swift
+//  AstronomyTests
+//
+//  Created by Jonathan T. Miles on 9/17/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class MarsRoverClientTest: XCTestCase {
+        
+    
+    
+}
